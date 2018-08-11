4cb267768f1534b6b4566853c756dc09816d36a0
Wabuluka <dwabuluka@gmail.com>
2018-08-11 23:55:07 +0300

    Homepage
    Adding other components onto the template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,37 +8,67 @@
                     <h1>Discover your talent</h1>
                     <p class="lead">The fact that these children are orphans does not deny the fact that God created them each with their talents</p>
 
-                    <p class="small alert-danger">Please note that this site is still under construction, check back for updates</p>
+                    <p class="small alert-danger">Please note that this website is still under construction, check back for updates</p>
                 </div>
-                <div class="col-4">
-                    <p>largeimage</p>
+                <div class="col-4 LanginPageRight text-right small">
+                    <p>Image not available now</p>
                 </div>
             </div>
         </div>
     </section>
     <section id="LandingPageSimpleIntro">
-        <div class="container">
+        <div class="container LandingPageSimpleIntro">
             <div class="row">
-                <div class="col-12">
-                    <p>LandingPageSimpleIntro</p>
+                <div class="col-12 mt-1 text-lg-center">
+                    <h1><strong>Goshen’s Fountain of Nutrition and Health</strong></h1>
+                    <p class="lead">Qui ut occaecat nostrud nisi eu incididunt incididunt magna dolore mollit in velit officia sunt reprehenderit enim ad excepteur tempor aute sint laboris ut ut.</p>
                 </div>
             </div>
         </div>
     </section>
     <section id="LangingPageVideoCard">
         <div class="container LangingPageVideoCard">
-            <div class="row">
+            <div class="row text-lg-center">
                 <div class="col-12">
-                    <p>LangingPageVideoCard</p>
+                    <div class="card-deck">
+                      <div class="card rounded-0">
+                        <div class="card-body">
+                          <h5 class="card-title">OUR MISSION</h5>
+                          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                      </div>
+                      <div class="card rounded-0">
+                        <div class="card-body">
+                          <h5 class="card-title">OUR VISSION</h5>
+                          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                      </div>
+                      <div class="card rounded-0">
+                        <div class="card-body">
+                          <h5 class="card-title">MOTTO</h5>
+                          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row text-lg-center mt-3">
+                <div class="col-12">
+                    <h4>Heading</h4>
                 </div>
             </div>
         </div>
     </section>
     <section id="LandingPageCallForDonation">
         <div class="container LandingPageCallForDonation">
-            <div class="row">
+            <div class="row text-lg-center">
                 <div class="col-12">
-                    <p>LandingPageCallForDonation</p>
+                    <form class="form-inline">
+                      <div class="form-group mx-sm-3 mb-2">
+                        <input type="email" class="form-control form-control-lg rounded-0" id="" placeholder="enter email address">
+                      </div>
+                      <button type="submit" class="btn mb-2 form-control-lg rounded-0">Subscribe</button>
+                    </form>
                 </div>
             </div>
         </div>
@@ -46,19 +76,77 @@
     <section id="LandingPageBlog">
         <div class="container LandingPageBlog">
             <div class="row">
-                <div class="col-12">
-                    <p>LandingPageBlog</p>
+                <div class="col-12 text-lg-center LandingPageBlogHeading">
+                    <h4>Our news</h4>
                 </div>
+            </div>
+            <div class="row">
+                <div class="row">
+                <div class="col-12">
+                    <div class="card-columns">
+                      <div class="card rounded-0">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title One</h5>
+                          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                      </div>
+                      <div class="card rounded-0">
+                        <div class="card-body">
+                          <h5 class="card-title">Post title Two</h5>
+                          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                      </div>
+                      <div class="card rounded-0">
+                        <div class="card-body">
+                          <h5 class="card-title">Post title Three</h5>
+                          <p class="card-text">This card has a regular title and short paragraphy of text below it.</p>
+                        </div>
+                      </div>
+                      <div class="card rounded-0">
+                        <div class="card-body">
+                          <h5 class="card-title">Post title Four</h5>
+                          <p class="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
+                        </div>
+                      </div>
+                      <div class="card rounded-0">
+                        <div class="card-body">
+                          <h5 class="card-title">Post title Five</h5>
+                          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                      </div>
+                      <div class="card rounded-0">
+                        <div class="card-body">
+                          <h5 class="card-title">Post title Six</h5>
+                          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                      </div>
+                      <div class="card rounded-0">
+                        <div class="card-body">
+                          <h5 class="card-title">Post title Seven</h5>
+                          <p class="card-text">This card has a regular title and short paragraphy of text below it.</p>
+                        </div>
+                      </div>
+                      <div class="card rounded-0">
+                        <div class="card-body">
+                          <h5 class="card-title">Post title Eight</h5>
+                          <p class="card-text">This card has a regular title and short paragraphy of text below it.</p>
+                        </div>
+                      </div>
+                      <div class="card rounded-0">
+                        <div class="card-body">
+                          <h5 class="card-title">Post title Nine</h5>
+                          <p class="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     </section>
     <section id="LandingPageClosure ">
         <div class="container LandingPageClosure">
-            <div class="row">
-                <div class="col-12">
-                    <p>LandingPageClosure</p>
-                </div>
-            </div>
+            
         </div>
     </section>
 @endsection
