a4407369665513bc06917c71005f7f647c42343e
Wabuluka <dwabuluka@gmail.com>
2018-08-17 12:13:03 +0300

    Another update

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -1,1 +1,46 @@
 @extends('layout')
+@section('content')
+
+<section id="contact">
+	<div class="container">
+      <div class="starter-template text-lg-center">
+        <h1>Goshen's Goal</h1>
+        <p class="lead">To empower individuals, Households and Communities with Resources for Sustainable food and Nutrition Security</p>
+      </div>
+
+      <div class="row">
+      	<div class="card-deck">
+      		<div class="card text-center rounded-0">
+			    <i class="fas fa-phone-square fa-7x"></i>
+			    <div class="card-body">
+			      <h5 class="card-title">Card title</h5>
+			      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			    </div>
+		  	</div>
+      		<div class="card text-center rounded-0">
+			    <i class="fas fa-building fa-7x mt-1"></i>
+			    <div class="card-body">
+			      <h5 class="card-title">Card title</h5>
+			      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			    </div>
+		  	</div>
+      		<div class="card text-center rounded-0">
+			    <i class="fas fa-envelope fa-7x"></i>
+			    <div class="card-body">
+			      <h5 class="card-title">Card title</h5>
+			      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			    </div>
+		  	</div>
+      </div>
+  </div>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-12 mt-3 mb-5">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3316.7886452410226!2d32.75264292345322!3d0.3670064840584216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177dc7cc1c430c79%3A0x145ebe890d25787d!2sMukono+-+Kayunga+Road%2C+Mukono!5e0!3m2!1sen!2sug!4v1534496679294" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+		</div>
+	</div>
+
+</section>
+@stop
